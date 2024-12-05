d792a6cb11402dd66e7aa97e03bd411aeb8514fa
JxxC0831 <jiaxuan@mac.mynetworksettings.com>
2024-12-05 16:45:25 -0500

    Add setup.php, Procfile, and composer.json for Heroku deployment

--- app/core/setup.php
+++ app/core/setup.php
@@ -10,14 +10,26 @@ require '../app/models/User.php';
 require '../app/models/QuizRecord.php';
 
  
-//set up env variables
-$env = parse_ini_file('../.env');
-
-define('DBNAME', $env['DBNAME']);
-define('DBHOST', $env['DBHOST']);
-define('DBUSER', $env['DBUSER']);
-define('DBPASS', $env['DBPASS']);
-define('DBDRIVER', '');
-
+if ($_SERVER['SERVER_NAME'] === 'localhost') {
+    // Local environment: Load configuration from .env file
+    $env = parse_ini_file('../.env');
+    /** Database configuration **/
+    define('DBNAME', $env['DBNAME']);
+    define('DBHOST', $env['DBHOST']);
+    define('DBUSER', $env['DBUSER']);
+    define('DBPASS', $env['DBPASS']);
+    define('DBPORT', $env['DBPORT']);
+} else {
+    // Production environment: Use Heroku Config Vars
+    /** Database configuration **/
+    define('DBNAME', getenv('DBNAME'));
+    define('DBHOST', getenv('DBHOST'));
+    define('DBUSER', getenv('DBUSER'));
+    define('DBPASS', getenv('DBPASS'));
+    define('DBPORT', getenv('DBPORT'));
+    
+    // Set the root URL for production
+    define('ROOT', 'https://rocky-tor-10358-918df2480069.herokuapp.com/');
+}
 //set up other configs
 define('DEBUG', true);
